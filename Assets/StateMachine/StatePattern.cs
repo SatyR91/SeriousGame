5369e72daf479e86d2d01fb22e72f5edf6ee8191
Nicolas Pierrot <pierrot.nicolas5@gmail.com>
2017-03-05 19:33:48 -0500

    IA V1
    tres basique

---
+++ Assets/StateMachine/StatePattern.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class StatePattern: MonoBehaviour
+{
+    public Transform deviceToUse;
+    public Transform bed;
+    public Transform outside;
+
+    [HideInInspector]
+    public float curTime;
+    [HideInInspector]
+    public IState currentState;
+    [HideInInspector]
+    public GoSleepState goSleepState;
+    [HideInInspector]
+    public SleepState sleepState;
+    [HideInInspector]
+    public GoOutState goOutState;
+    [HideInInspector]
+    public OutState outState;
+    [HideInInspector]
+    public GoUseState goUseState;
+    [HideInInspector]
+    public UseState useState;
+    
+
+
+    public NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        goSleepState = new GoSleepState(this);
+        sleepState = new SleepState(this);
+        goOutState = new GoOutState(this);
+        outState = new OutState(this);
+        goUseState = new GoUseState(this);
+        useState = new UseState(this);
+
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        currentState = goOutState;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState.UpdateState();
+    }
+}
---
+++ Assets/StateMachine/UseState.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class UseState : IState
+
+{
+    private readonly StatePattern guy;
+
+    public UseState(StatePattern statePatternGuy)
+    {
+        guy = statePatternGuy;
+    }
+
+    public void UpdateState()
+    {
+        Use();
+    }
+
+    public void ToGoSleepState()
+    { }
+
+    public void ToSleepState()
+    { }
+
+    public void ToGoOutState()
+    { }
+
+    public void ToOutState()
+    { }
+
+    public void ToGoUseState()
+    {
+        guy.currentState = guy.goUseState;
+    }
+
+    public void ToUseState()
+    { }
+
+    public void Use()
+    {
+        if (Time.time - guy.curTime >= 2)
+        {
+            ToGoUseState();
+        }
+    }
+}
