9647b06ca8e8146dadb1d708da6cdbc7d84d2705
Aimé Yamamoto <aimeyamamoto@gmail.com>
2017-03-06 20:28:50 -0500

    Clock Game Time
    GameTime (Day = 5 Min)

---
+++ Assets/DigitalClock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DigitalClock : MonoBehaviour {
+
+    private string text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var date = System.DateTime.Now;
+        text = date.ToString("HH:mm:ss");
+        GetComponent<TextMesh>().text = text;
+	}
+     
+}
+
---
+++ Assets/DigitalGameTimeClock.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DigitalGameTimeClock : MonoBehaviour
+{
+
+    float nextSecond = 12.5f/60f;
+    int second;
+    float minute = 0.0f;
+    float hour = 0.0f;
+    float day = 0.0f;
+    float month = 0.0f;
+    float year = 0.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nextSecond > 0)
+        {
+            nextSecond -= Time.deltaTime;
+        }
+        else
+        {
+            nextSecond = 12.5f/60f;
+            minute += 1;
+        }
+        if(minute >= 60)
+        {
+            minute = 0;
+            hour += 1;
+        }
+        if (hour >= 24)
+        {
+
+            hour = 0;
+
+            day += 1;
+
+        }
+
+        if (day >= 30)
+        {
+
+            day = 0;
+
+            month += 1;
+
+        }
+
+        if (month >= 12)
+        {
+
+            month = 0;
+
+            year += 1;
+
+        }
+
+        GetComponent<TextMesh>().text =  String.Format("{0:00}:{1:00}", hour,minute);
+
+    }
+}
