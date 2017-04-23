5244e0ac77b4f249eecf083f13cc4da0cdd2de1c
Moonvibes <antoine.ruffion@gmail.com>
2017-04-23 02:42:03 -0400

    Upgrade Descriptions

--- Assets/Scripts/FamilyMemberUpgrade/FamilyUpgradeButton.cs
+++ Assets/Scripts/FamilyMemberUpgrade/FamilyUpgradeButton.cs
@@ -48,7 +48,20 @@ public class FamilyUpgradeButton : MonoBehaviour {
                 child.gameObject.GetComponent<FamilyUpgradeButton>().isActive = false;
             }
         }
-        UpgradeDescriptionText.text = UpgradeName;
+        switch(UpgradeName)
+        {
+            case ("Light"):
+                UpgradeDescriptionText.text = "The easiest way to spend less on electricity is to be cautious : turning the lights off when you go out of a room can decrease your energy expenditure drastically. \nInvesting points in this upgrade will reduce the chance that family members forget to turn off the light when going out of a room.";
+                break;
+            case ("Moral"):
+                UpgradeDescriptionText.text = "Most of the time, people don't use their entertainment devices to their full extent. By educating the family about all the possibility of their devices, it will become easier for them to have fun with them. \nInvesting points in this upgrade will cause family members to gain more moral when using entertainment devices.";
+                break;
+            case ("Energy"):
+                UpgradeDescriptionText.text = "Being mindful of small things, like truly turning off each device after use, or unplugging them after we finished working with them, can help save a bit of energy. \nInvesting points in this upgrade will cause home appliances to be used in a more responsible manner, thus using less energy.";
+                break;
+            default:
+                break;
+        }
         CheckForUpgrade();
     }
 
